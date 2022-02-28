f1600fd29252c2acd4761f191cb5f14803ccfb5f
fuck your bitch <66913721+eazyeee@users.noreply.github.com>
2022-02-28 10:10:23 -0300

    chat msg to booth

--- booth name to msg in chat.lua
+++ booth name to msg in chat.lua
@@ -1,3 +1,4 @@
+--basically converts all msgs in chat into your boot, you get alot of attention using this lmao
 for i, v in pairs(game.Players:GetChildren()) do 
     v.Chatted:Connect(function(m)
         local args = {
